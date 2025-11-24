d78f4055633ffcf046409a63f13ccd506b24fe6d
Philip <philiptap2005@outlook.com>
2025-11-24 22:06:40 +0100

    Calander types

--- starting-framework/src/Calendar.hs
+++ starting-framework/src/Calendar.hs
@@ -7,9 +7,24 @@ import ParseLib.Derived
 
 -- Exercise 6
 data Calendar = Calendar
+  {
+    version :: String,
+    prodID :: String,
+    events :: [Event]
+  }
   deriving (Eq, Ord, Show)
 
+-- Even is a data type that has a few required entries and a few optional ones. The order of the contents does not matter to the event
 data Event = Event
+  {
+    stampDate :: DateTime, --Required
+    uid :: String,         --Required
+    startDate :: DateTime, --Required
+    endDate :: DateTime,   --Required
+    description :: String, --Optional
+    summary :: String,     --Required
+    location :: String     --Optional
+  }
   deriving (Eq, Ord, Show)
 
 -- If you plan on using your own types in Calendar, Event, or Token. Make sure it derives Eq, Generic, and CustomData.
