c5ccc2db99ac6c798907632493a454cef6470f60
Philip <philiptap2005@outlook.com>
2025-11-25 20:54:23 +0100

    Hopefully working lexer

--- starting-framework/src/Calendar.hs
+++ starting-framework/src/Calendar.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+
 module Calendar where
 
 import DateTime
 import GHC.Generics (Generic)
 import GradeLib.CustomASTData (CustomData)
 import ParseLib.Derived
+import Data.Char as Char
+import Control.Applicative ((<|>))
+import Data.Functor
 
 -- Exercise 6
 data Calendar = Calendar
@@ -40,10 +46,11 @@ data Token = Token
   }
   deriving (Eq, Ord, Show)
 
-data Header = PRODID | VERSION | DTSTAMP | UID | DTSTART | DTEND | SUMMARY | DESCRIPTION | LOCATION deriving (Enum, Eq, Ord, Show, Generic, CustomData)
+data Header = PRODID | VERSION | EVENT | DTSTAMP | UID | DTSTART | DTEND | SUMMARY | DESCRIPTION | LOCATION deriving (Enum, Eq, Ord, Show, Generic, CustomData)
 -- List of token is for storing an event
-data Content = DateTime DateTime | String String | Tokens [Token] deriving (Eq, Ord, Show, Generic, CustomData)
+data Content = Timestamp DateTime | String String | Tokens [Token] deriving (Eq, Ord, Show, Generic, CustomData)
 
+-- Note cannot retreive EVENT enum from get header, as not done through string
 getHeader :: String -> Header
 getHeader "PRODID" = PRODID
 getHeader "VERSION" = VERSION
@@ -55,11 +62,49 @@ getHeader "SUMMARY" = SUMMARY
 getHeader "DESCRIPTION" = DESCRIPTION
 getHeader "LOCATION" = LOCATION
 
---TODO
---Split on newlines
---To read content, use DropWhile (!= :) (because we can have multiple : so split does not work)
+newline :: Parser Char Char
+newline = symbol '\n'
+
+parseBeforeColon :: Parser Char String
+parseBeforeColon = greedy (satisfy (/=':'))
+
+colon :: Parser Char Char
+colon = symbol ':'
+
+parseHeader :: Parser Char Header
+parseHeader = getHeader <$> some (satisfy Char.isAlpha)
+
+parseContent :: Header -> Parser Char Content
+parseContent header =
+  case header of
+    DTSTAMP -> Timestamp <$> parseDateTime
+    DTSTART -> Timestamp <$> parseDateTime
+    DTEND   -> Timestamp <$> parseDateTime
+    _       -> String   <$> greedy (satisfy (/='\n'))
+
+parseLine :: Parser Char Token
+parseLine = do
+  key <- parseBeforeColon
+  _   <- colon
+  let header = getHeader key
+  content   <- parseContent header
+  _   <- newline
+  pure (Token header content)
+
+parseEvent :: Parser Char Token
+parseEvent = do
+  _ <- token "BEGIN:VEVENT" *> newline
+  tokens <- many parseLine
+  _ <- token "END:VEVENT" *> newline
+  pure (Token EVENT (Tokens tokens))
+
 lexCalendar :: Parser Char [Token]
-lexCalendar = undefined
+lexCalendar = do
+  _ <- token "BEGIN:VCALENDAR" *> newline
+  tokens <- many (parseEvent <|> parseLine)
+  _ <- ((token "END:VCALENDAR" *> newline) $> ()) <|> (token "END:VCALENDAR" $> ())
+  pure tokens
+
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = undefined
